48bb968442c55c6adb03711d83d18c46bf715068
TreusK <flutri@protonmail.com>
2021-12-18 21:27:58 -0300

    Fighting with css and forms

---
+++ README.md
@@ -0,0 +1,3 @@
+Finally doing a todo, I hate forms and css and a todo is all form all css 
+
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,77 @@
-let todosList = document.querySelector('#todosList');
+//DOM grabbin'
+let todosContainer = document.querySelector('#todosContainer');
+let containerForm = document.querySelector('#containerForm');
+let newTodoBtn = document.querySelector('#newTodoBtn');
+let newProjectBtn = document.querySelector('#newProjectBtn');
+let form = document.querySelector('#form');
 
-function todoFactory(name, description, date) {
+let arrOfTodos = [];
 
-    return {name, description, date}
-}
 
-function render(parent, arr) {
-    parent.innerHTML = arr;
-}
+//ToDo module
+const todoMaker = (() => {
+    //Private function to render entire content
+    function render(parent, elem) {
+        parent.innerHTML = elem;
+    }
 
-let todoObj = {
-    title: 'Study french',
-    description: 'Open the french book and read page 287 for 1 hour, then consoom french content for 30m',
-    dueDate: '13/07/21',
-    priority: 0,
-}
+    //Function to empty the todos container
+    function reset() {
+        render(todosContainer, '');
+    }
 
-//Quiero renderear 1 obj/todo en la pag, que hago
-//Necesito una funcion q haga obj -> html notations so innerhtml
-function objToTodo(obj) {
-    let todo = `<div><span>${obj.title}</span> <span>${obj.dueDate}</span> <span>${obj.priority}</span></div>`;
-    render(todosList, todo)
-}
+    //Private function to take the form and return a todo object
+    function formToObj() {
 
-objToTodo(todoObj)
+    }
+
+    //Private function to take an object and return an html todo element
+    function objToHtml(obj) {
+        let todo = document.createElement('div');
+        todo.classList.add('todo');
+        todo.innerHTML = `<span>${obj.title}</span> <span>${obj.dueDate}</span> <span>${obj.priority}</span>`;
+        return todo;
+    }
+
+    //Function to render the arrOfTodos
+    function renderArr() {
+        arrOfTodos.map(elem => todosContainer.append(objToHtml(elem)));
+    }
+
+    return {reset, renderArr}
+})();
+
+//Form module
+const formModule = (() => {
+    function hi() {
+        console.log('hi');
+    }
+    return {hi}
+})();
+
+/////Events
+
+//Event to add visibility of the form container
+newTodoBtn.addEventListener('click', (e) => containerForm.classList.add('showMe'));
+
+//Event to stop displaying the form container
+containerForm.addEventListener('click', (e) => {
+    if(e.target.id == 'containerForm') {
+        containerForm.classList.remove('showMe');
+    }
+});
+
+//Event on form submit
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    //I could grab the e.target.childrn elements I guess
+
+    //Also there is a reset method already available
+    //form.reset();
+})
+
+
 
 
    
